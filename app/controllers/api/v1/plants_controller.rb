aeb6023b9ba5c95b81711624fd9096d759571db5
BlythePollard <github email address>
2020-05-01 17:39:55 -0600

    modified plants_controller to be linked to user

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PlantsController < ApplicationController
 
     def create
-        @plant = Plant.new(plants_params)
+        @plant = @user.plants.build(plants_params)
         if @plant.save
             render json: @plant
         else 
@@ -10,13 +10,13 @@ class Api::V1::PlantsController < ApplicationController
     end
 
     def index
-        @plants = Plant.all
+        @plants = @user.plants.all
         render json: @plants
         #will want to use serializer if we don't want to send all data to front end (or use 'except')
     end
 
     def show
-        @plant = Plant.find(params[:id]) #repetetive- can make a before set_account method if I want to refactor
+        @plant = @user.plants.find(params[:id]) #repetetive- can make a before set_account method if I want to refactor
         render json: @plant
     end
 
@@ -29,6 +29,10 @@ class Api::V1::PlantsController < ApplicationController
 
     def plants_params
         params.require(:plant).permit(:name, :description, :available, :user_id)
+    end
+
+    def set_user
+        @user = User.find(params[:user_id])
     end
 
 end
