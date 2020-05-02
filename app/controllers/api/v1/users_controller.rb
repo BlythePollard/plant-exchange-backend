abfcf5c5d4fd3643559c65b083a8a4ddcd3d07af
BlythePollard <github email address>
2020-05-02 16:26:21 -0600

    added basic actions, reducers, notes in app.js

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,11 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    def index
+        users = User.all 
+        render json: users
+    end
+
     def show
         user = User.find_by(id: params[:id])
         if user
